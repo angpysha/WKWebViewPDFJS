b702b7d65c5e9c2263f4660f0d4ad8bbcfb5030f
Andrii Petrovskyi <andrii.petrovskyi@sp.woodplc.com>
2022-06-12 14:43:44 +0300

    Initial Commit

---
+++ TestPDFViewer2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestPDFViewer2
+//
+//  Created by Andrii on 12.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
